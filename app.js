ad8586a859c23e2aa05f8367c204028d16ee9ae9
enigma-dy <oluwagbengaadedire@gmail.com>
2025-05-09 03:35:56 +0100

    Added Status Code for  No Content Route

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app =express();
 // Create your module in routeModule.js
 // add your module below e.g {indexRoute, yourNewModule}
-const {indexRoute} = require("./custom-module/routeModule");
+const {indexRoute,noContentRoute} = require("./custom-module/routeModule");
+
 const port = 5000;
 // Middleware
 app.use(express.json());
 
 // Follow the format below to create your route and your module as defined in routeModule.js file
-app.get("/", indexRoute)
-
+app.get("/", indexRoute);
+app.get("/no-content", noContentRoute);
 
 // Server 
 app.listen(port, () => {
--- custom-module/routeModule.js
+++ custom-module/routeModule.js
@@ -1,9 +1,15 @@
 
 // Follow the format below to create your module according to your specification
 // Dont forget to use diffrent variable from the existing variable
+
 const indexRoute = (req, res) => {
     res.send("Welcome to group 3 mini project");
 }
 
+
+const noContentRoute =(req,res)=>{
+    res.status(204).send()
+}
 // export your module here e.g {indexRoute, yourNewModule}
-module.exports = {indexRoute};
+module.exports = {indexRoute,noContentRoute};
+
