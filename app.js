d75e0452f74aa9d5ed6f0cd1d1a59fe7f42142c9
Racheal Frontend developer <destinifeoluwayemisi@gmail.com>
2025-05-11 12:50:30 +0100

    500 Server Error

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-
 import express from 'express';
 const app = express();
 // Create your module in routeModule.js inside custom-module folder
 // add your module below e.g {indexRoute, yourNewModule}
+import serverError from './custom-module/500error.js';
 import {indexRoute} from './custom-module/routeModule.js';
 const port = 5000;
 // Middleware
 app.use(express.json());
+app.use('/internal-error', serverError);
 
 // Follow the format below to create your route and your module as defined in routeModule.js file
-app.get("/", indexRoute)
+// routes 500 error
+app.get("/", indexRoute);
+app.use("/internal-error", serverError);
 
 
 // Server 
