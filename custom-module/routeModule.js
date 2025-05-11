8c431f578691d16909b04cd920aeb4d5fb557c06
Arthakker <arthuronyeanusi@gmail.com>
2025-05-11 19:34:36 +0100

    Completed status 400 code for handling invalid form data

--- custom-module/routeModule.js
+++ custom-module/routeModule.js
@@ -1,9 +1,31 @@
-
 // Follow the format below to create your module according to your specification
 // Dont forget to use diffrent variable from the existing variable
-const indexRoute = (req, res) => {
-    res.send("Welcome to group 3 mini project");
-}
 
+//Imported express, express middleware using modules according to ejs
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const indexRoute = (req, res) => {
+  res.send("Welcome to group 3 mini project");
+};
+
+//Handling Incomplete, invalid or broken responses #statusCode: 400
+
+const validateResponse = (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).send("Response incomplete, All fields are required");
+  }
+
+  if (name === "" || email === "" || password === "") {
+    res.status(400).send("Fields cannot be empty");
+  }
+};
 // export your module here e.g {indexRoute, yourNewModule}
-      export {indexRoute};
+export { indexRoute };
+export { validateResponse };
