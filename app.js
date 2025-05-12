ec8d467f8f75c86951a9b29a88784f422e52c88c
Gracialla <ojirikagracious@gmail.com>
2025-05-12 10:42:45 +0100

    Added 200 OK satus route

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import express from 'express';
 const app = express();
 // Create your module in routeModule.js inside custom-module folder
 // add your module below e.g {indexRoute, yourNewModule}
-import {indexRoute} from './custom-module/routeModule.js';
+import {route200} from './custom-module/routeModule.js';
 const port = 5000;
 // Middleware
 app.use(express.json());
 
 // Follow the format below to create your route and your module as defined in routeModule.js file
-app.get("/", indexRoute)
+app.get("/200", route200)
 
 
 // Server 
--- custom-module/routeModule.js
+++ custom-module/routeModule.js
@@ -1,9 +1,11 @@
-
-// Follow the format below to create your module according to your specification
-// Dont forget to use diffrent variable from the existing variable
-const indexRoute = (req, res) => {
-    res.send("Welcome to group 3 mini project");
-}
-
 // export your module here e.g {indexRoute, yourNewModule}
       export {indexRoute};
+      // routeModule.js
+const route200 = (req, res) => {
+  res.status(200).json({
+    title: '200 OK',
+    description: 'The request has succeeded.'
+  });
+};
+
+export { route200 };
